a11664985fb0951041c9054103b2626475ffb390
Andrey Isenko <aisenko87@gmail.com>
2025-02-16 20:39:34 +0200

    add api

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,24 @@
+import { useEffect } from 'react';
+import  fetchArticles  from '../services/api'
 import './App.css'
+
+
 
 function App() {
 
+
+  useEffect(() => {
+    const getData = async () => {
+      const data = await fetchArticles();
+      console.log(data);
+    };
+    getData()
+    
+  }, []);
+
+
+
+  
   return (
     <>
      <h1>goit-react-hw-05</h1>
